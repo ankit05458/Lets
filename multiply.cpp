8d27c9452d56ae378a31efb21baef4d9edff6431
Ankit_Lohani_25 <ankitlohani@oakter.com>
2022-10-18 16:03:24 +0530

    adding multiply file  multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int a;
+    int b;
+    int product=1;
+    cout<<"enter the number\ n";
+    cin>>a>>b;
+    product = a*b;
+
+    cout<<"The product is "<<product<<endl;
+
+
+    return 0;
+
+
+
+
+}
