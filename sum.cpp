a2d8924f86869cdff2e3cd8e26bcb3e7095a9d8a
Ankit_Lohani_25 <ankitlohani@oakter.com>
2022-10-18 12:42:23 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+int sum ;
+int a, b;
+
+cout<<"enter the first number"<<endl;
+cin>>a;
+
+cout<<"enter the second number"<<endl;
+cin>>b;
+
+sum =a+b;
+
+cout<<"the sum is  "<<sum<<endl;
+
+
+    return 0;
+}
