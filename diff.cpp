795ef25940242a1cf1e9262f3c95f9898fa40d08
Ankit_Lohani_25 <ankitlohani@oakter.com>
2022-10-19 11:39:09 +0530

    Add implementaion for diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main()
 {
+    int a,b;
+    int diff;
+cout<<"enter  two numbers\n"<<endl;
+cin>>a>>b;
+diff = a-b;
 
-cout<<"the difference Program"<<endl;
+cout<<"the difference Program"<<diff<<endl;
     return 0;
 }
